65d5b0ff96bebc6a23c71de302742d0ae066b66d
jee-in <chris309804@gmail.com>
2025-03-08 20:32:09 +0900

    chore: Add configuration to use Spring Config Server

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
