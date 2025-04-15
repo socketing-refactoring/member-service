14b0a49321c9f94ac5fe75eca9deba70acb5709a
jee-in <chris309804@gmail.com>
2025-04-15 14:49:34 +0900

    fix: Fix wrong api path

--- src/main/java/com/jeein/member/controller/ManagerController.java
+++ src/main/java/com/jeein/member/controller/ManagerController.java
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/managers/managers")
+@RequestMapping("/api/v1/managers")
 @RequiredArgsConstructor
 public class ManagerController {
 
